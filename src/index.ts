b1c04d80a3b915f9273749efd17de41c1c9418fa
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-07-27 19:00:03 -0300

    Setup documentation with swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
+import openApiConfiguration from "../openapi.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(openApiConfiguration));
 
 app.use("/users", usersRoutes);
 
